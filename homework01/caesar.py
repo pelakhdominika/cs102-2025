fecaee493aa303290dc4eabc6638c859e6a93141
pelakhdominika <pelakh2707@gmail.com>
2025-10-28 04:02:29 +0600

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,4 @@
-def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+def encrypt_caesar(plaintext: str, shift = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
     >>> encrypt_caesar("PYTHON")
@@ -11,13 +11,41 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    if (plaintext == ""):
+        ciphertext = ""
+    else:
+        for  i in range(len(plaintext)):
+                f = False
+                sym = ord(plaintext[i])
+                n_sym = 0
+            
+                
+                if 65 <= sym <= (90 - shift): 
+                    n_sym = sym + shift
+                    f = True
+                elif ((90 - shift) <= sym <= (90 + shift)):
+                    n_sym = (65 + sym - 88)
+                    f = True
+                if ( (f == False) and (97 <= sym <= (122 - shift)) ):
+                    n_sym = sym + shift
+                    f = True
+                elif ( (f == False) and ((122 - shift) <= sym <= (122 + shift)) ):
+                    n_sym = 97 + (sym - 119) - 1
+                    f = True
+                    
+                if (f == False):
+                    n_sym = sym
+
+                ciphertext += chr(n_sym)
     return ciphertext
+        
+
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +56,36 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    if (ciphertext == ""):
+        plaintext = ""
+    else:
+        for  i in range(len(ciphertext)):
+                f = False
+                sym = ord(ciphertext[i])
+                n_sym = 0
+            
+                
+                if ((65 + shift) <= sym <= 90): 
+                    n_sym = sym - shift
+                    f = True
+                elif ((65 - shift) <= sym <= (65 + shift - 1)):
+                    n_sym = (65 + (90 - sym))
+                    f = True
+                if ( (f == False) and ((97 + shift) <= sym <= 122) ):
+                    n_sym = sym - shift
+                    
+                    f = True
+                elif ( (f == False) and (97 - shift) <= sym <= (97 + shift - 1) ):
+                    n_sym = (97 + (122 - sym))
+                    f = True
+                    
+                if (f == False):
+                    n_sym = sym
+
+                plaintext += chr(n_sym)
+    
     return plaintext
+
+
+
+
