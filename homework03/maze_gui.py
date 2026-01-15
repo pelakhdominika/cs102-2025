0464674572a9a043d81c66c70422cbff670b7ba9
pelakhdominika <pelakh2707@gmail.com>
2026-01-15 13:44:05 +0600

    Update maze_gui.py

--- homework03/maze_gui.py
+++ homework03/maze_gui.py
@@ -1,52 +1,53 @@
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import ttk
 from typing import List
 
 from maze import add_path_to_grid, bin_tree_maze, solve_maze
 
-CELL_SIZE = 10
+csize = 10
+N, M = 51, 77
 
 
-def draw_cell(x, y, color, size: int = CELL_SIZE):
+def draw_cell(x, y, color, size: int = 10):
     x *= size
     y *= size
-    canvas.create_rectangle(x, y, x + size, y + size, fill=color)
+    x1 = x + size
+    y1 = y + size
+    canvas.create_rectangle(x, y, x1, y1, fill=color)
 
 
-def draw_maze(grid: List[List[str]], size: int = CELL_SIZE):
-    canvas.delete("all")
+def draw_maze(grid: List[List[str]], size: int = 10):
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
-            color = "white" if cell == " " else "black" if cell == "■" else "purple"
+            if cell == " ":
+                color = "White"
+            elif cell == "■":
+                color = "black"
+            else:
+                color = "green"
             draw_cell(y, x, color, size)
 
 
-def show_solution():
-    maze_copy, path = solve_maze(GRID)
-    maze_copy = add_path_to_grid(GRID, path)
-    if path:
-        draw_maze(maze_copy, CELL_SIZE)
-    else:
-        messagebox.showinfo("Message", "No solution")
+def show_solution(displayed_maze, displayed_path):
+    displayed_maze = add_path_to_grid(GRID, displayed_path)
+    draw_maze(displayed_maze, csize)
 
 
 if __name__ == "__main__":
-    N, M = 51, 77
-    GRID = bin_tree_maze(N, M)
-    _, PATH = solve_maze(GRID)
-    while not PATH:
+    has_solution = False
+    while not has_solution:
         GRID = bin_tree_maze(N, M)
-        _, PATH = solve_maze(GRID)
+        maze, path = solve_maze(GRID)
+        has_solution = bool(path)
 
     window = tk.Tk()
     window.title("Maze")
-    window.geometry(f"{M * CELL_SIZE + 100}x{N * CELL_SIZE + 100}")
+    window.geometry("%dx%d" % (M * csize + 100, N * csize + 100))
 
-    canvas = tk.Canvas(window, width=M * CELL_SIZE, height=N * CELL_SIZE)
+    canvas = tk.Canvas(window, width=M * csize, height=N * csize)
     canvas.pack()
 
-    draw_maze(GRID, CELL_SIZE)
-    ttk.Button(window, text="Solve", command=show_solution).pack(pady=20)
+    draw_maze(GRID, csize)
+    ttk.Button(window, text="Solve", command=lambda: show_solution(maze, path)).pack(pady=20)
 
     window.mainloop()
-
